0962f0c78bb0041f85c5082abccc416fece2d95c
Eva Ying <Eva.Ying@bofifederalbank.com>
2025-01-06 11:03:33 -0800

    Initial commit

---
+++ bank.py
@@ -0,0 +1,5 @@
+class BankAccount:
+    def __init__(self, initialAmount, acctName):
+        self.balance = initialAmount
+        self.name = acctName
+        print(f"\nAccount '{self.name}' has Balance = ${self.balance:.2f}")
