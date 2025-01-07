b92589765a074359ec8329f707493615b82b93bd
Eva Ying <Eva.Ying@bofifederalbank.com>
2025-01-07 08:53:31 -0800

    Updated code with custom BalanceException for withdrawal logic

--- bank.py
+++ bank.py
@@ -1,5 +1,46 @@
+class BalanceException(Exception):
+    pass
+
 class BankAccount:
     def __init__(self, initialAmount, acctName):
         self.balance = initialAmount
         self.name = acctName
-        print(f"\nAccount '{self.name}' has Balance = ${self.balance:.2f}")
+        print(f"\nAccount '{self.name}' created. \n Balance = ${self.balance:.2f}")
+
+
+    def get_balance(self):
+        print(f"\nAccount '{self.name} balance = ${self.balance:.2f}")
+
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount 
+        print(f"\n Deposit complete. \nAccount '{self.name} balance = ${self.balance:.2f}")
+        self.get_balance()
+
+    def viableTransaction(self,amount):
+        if self.balance >= amount:
+            return 
+        else:
+            raise BalanceException(
+                f"\nSorry, account '{self.name}' only has a balance of ${self.balance:.2f}"
+            )
+        
+    def withdraw(self,amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount 
+            print("\nWithdraw complete. ")
+            self.get_balance()
+        except BalanceException as Error:
+            print(f"\nWithdraw interrupted:{Error}")
+
+
+    def transfer(self, amount, account):
+        try:
+            print("\n****************\n \nBeginning Transfer...")
+            self.viableTransaction(amount)
+            self.withdraw(amount)
+            account.deposit(amount)
+            print("\nTransfer complete \n**********************")
+        except BalanceException as Error:
+            print(f"\nTransfer failed")
--- oop_bank.py
+++ oop_bank.py
@@ -1,3 +1,14 @@
 from bank import *
 
 Eva = BankAccount(1000, "Eva")
+Jeff = BankAccount(1000000, "Jeff")
+
+# Eva.get_balance()
+# Jeff.get_balance()
+
+Eva.deposit(1000)
+Jeff.deposit(2000)
+
+Eva.withdraw(50000)
+
+Jeff.transfer(60000,Eva)
